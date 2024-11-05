72058f14f4b3675db3dd210b7528f120797cbc47
M4rakesh <maksims.rogozins@gmail.com>
2024-11-05 11:57:28 +0200

    Update projekts copy.py
    qwer

--- projekts copy.py
+++ projekts copy.py
@@ -2,6 +2,51 @@ import itertools
 import datetime
 from datetime import time
 
+class Klients:
+    Klienta_vaards=""
+    Klienta_uzvaards=""
+    Klienta_PK=""
+    Klienta_tel_numurs=""
+    
+    id_iter_kl = itertools.count()
+
+    def __init__(self,_vaards,_uzvaards,_pk,_tel_numurs):
+        self.Klienta_id = next(self.id_iter_kl) + 1
+        self.Klienta_vaards=_vaards
+        self.Klienta_uzvaards=_uzvaards
+        self.Klienta_PK=_pk
+        self.Klienta_tel_numurs=_tel_numurs
+
+    def Klienta_info(self):
+        return [self.Klienta_id,self.Klienta_vaards,self.Klienta_uzvaards,self.Klienta_PK,self.Klienta_tel_numurs]
+
+    def Klientu_info_print(self):
+        print("Klienta vārds: "+ str(self.Klienta_vaards))
+        print("Klienta uzvārds: " + str(self.Klienta_uzvaards))
+        print("Klienta personas kods : "+ str(self.Klienta_PK))
+        print("Klienta Tel. numurs : " + str(self.Klienta_tel_numurs)) 
+
+Klient=[]
+with open("Klienti.txt","a",encoding="utf-8") as fail:
+    while True:
+        
+
+        again=input("Vai jūs gribat ivadiet vēl vienu personu: jā/nē ")
+        if again =="jā":
+            CilvekaVards=(input("Ievadiet savu vardu: "))
+            CilvekaUzards=(input("Ievadiet savu uzvardu: "))
+            PersonKod=(input("Ievadiet savu personas koda: "))
+            TelNr=(input("Ievadiet savu telefonu: "))
+            kli1=Klients(CilvekaVards,CilvekaUzards,PersonKod,TelNr)
+            print(kli1.Klienta_id)
+            kli1.Klientu_info_print()   
+            
+            fail.write(f"Klienta vards: {CilvekaVards} Klienta uzvards: {CilvekaUzards} Klienta personas kods: {PersonKod} Klienta telefona numurs:{TelNr}"+"\n")  
+            
+            
+        else:
+            print("Paldies par uzmanibu!")
+            break
 class Laukums:
     
     Platums_gridai = 0
@@ -32,80 +77,28 @@ class Laukums:
         print("Garums gridai: " + str(self.Garums_gridai))
         print("Laukums gridai: "+ str(self.Laukuma))
         
-    
-class Klients:
-    Klienta_vaards=""
-    Klienta_uzvaards=""
-    Klienta_PK=""
-    Klienta_tel_numurs=""
-    
-    id_iter_kl = itertools.count()
 
-    def __init__(self,_vaards,_uzvaards,_pk,_tel_numurs):
-        self.Klienta_id = next(self.id_iter_kl) + 1
-        self.Klienta_vaards=_vaards
-        self.Klienta_uzvaards=_uzvaards
-        self.Klienta_PK=_pk
-        self.Klienta_tel_numurs=_tel_numurs
-
-    def Klienta_info(self):
-        return [self.Klienta_id,self.Klienta_vaards,self.Klienta_uzvaards,self.Klienta_PK,self.Klienta_tel_numurs]
-
-    def Klientu_info_print(self):
-        print("Klienta vārds: "+ str(self.Klienta_vaards))
-        print("Klienta uzvārds: " + str(self.Klienta_uzvaards))
-        print("Klienta personas kods : "+ str(self.Klienta_PK))
-        print("Klienta Tel. numurs : " + str(self.Klienta_tel_numurs)) 
-
-'''class Izmantosana:
-    Pakalpojuma_saakuma_laiks= 0
-    Pakalpojuma_beigu_laiks=0
-    Pakalpojuma_datums=0
-    Izmantosdana_cena_stunda=10
-    id_pakalpojums=0
-    id_klients=0
-    Izmantosana_id=0
-
-    id_iter_izmantosana= itertools.count()
-
-    def Cena_kopa(self):
-        kopeja_cena=self.Izmantosdana_cena_stunda*(((self.Pakalpojuma_beigu_laiks - self.Pakalpojuma_saakuma_laiks)))
-        return kopeja_cena
-    
-    def Izmantosana_info_print(self):
-        print("Pakalpojuma sakuma laiks:"+ str(self.Pakalpojuma_saakuma_laiks))
-        print("Pakalpojuma beigu laiks:"+ str(self.Pakalpojuma_beigu_laiks))
-        print("Pakalpojums id:"+ str(self.id_pakalpojums))
-        print("Klients id:"+ str(self.id_klients))
-        print("Pakalpojuma cena stunda, EUR:"+str(self.Izmantosdana_cena_stunda)+"\n")'''
-
-
+class Material:
+    Laminats=''
+    Flize=''
+    Linolejs=''
+    Garums=0
+    Platums=0
+    Daudzums=0
+    def __init__(self,gar,plat,daudz):
+        self.Garums=gar
+        self.Platums=plat
+        self.Daudzums=daudz
+        
+    def Material_info(self):
+        int(input("Kadu materialu jūs izvelejas: (Linolejs,Frizes,Lamināts)"))
+    def Material_info_print():
+        
 a=int(input("Ievadiet garumu: "))
 b=int(input("Ievadit platumu: "))
 s1=Laukums(b,a)
 s1.aprekinasana()
 s1.Laukuma_info_print()
-Klient=[]
-with open("Klienti.txt","a",encoding="utf-8") as fail:
-    while True:
-        
-
-        again=input("Vai jūs gribat ivadiet vēl vienu personu: jā/nē ")
-        if again =="jā":
-            CilvekaVards=(input("Ievadiet savu vardu: "))
-            CilvekaUzards=(input("Ievadiet savu uzvardu: "))
-            PersonKod=(input("Ievadiet savu personas koda: "))
-            TelNr=(input("Ievadiet savu telefonu: "))
-            kli1=Klients(CilvekaVards,CilvekaUzards,PersonKod,TelNr)
-            print(kli1.Klienta_id)
-    #kli1.Klienta_info()
-            kli1.Klientu_info_print()   
-            
-            fail.write(f"Klienta vards: {CilvekaVards} Klienta uzvards: {CilvekaUzards} Klienta personas kods: {PersonKod} Klienta telefona numurs:{TelNr}"+"\n")  
-            
-            #for klienti in Klient:
-                #fail.write(klienti + "\n")
-        else:
-            print("Paldies par uzmanibu!")
-            break
     
+
+#https://github.com/M4rakesh/uzd/blob/main/mechta.py
