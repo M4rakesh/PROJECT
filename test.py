003cf51b482b8059f1438f1b3fe4c1623990198f
M4rakesh <maksims.rogozins@gmail.com>
2024-12-04 14:06:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,36 @@
-import numpy as np
-import cv2 as cv
-import imutils
 import cv2
+import numpy as np
 
-# параметры цветового фильтра
-hsv_min = np.array((0, 54, 5), np.uint8)
-hsv_max = np.array((187, 255, 253), np.uint8)
-img = cv.imread("C:\\45\\000.jpg")
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV )
-# меняем цветовую модель с BGR на HSV
-thresh = cv.inRange(hsv, hsv_min, hsv_max )
-# применяем цветовой фильтр
-# ищем контуры и складируем их в переменную contours
-contours, hierarchy = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-#попытка нарисовать хотябы линию на картинке (не работает)
-c = max(contours, key = cv.contourArea)
-M = cv2.moments(c)
-if M["m00"] != 0:
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    print (cX,cY)
-else:
-    cX, cY = 0,0
-    print (cX,cY)
+# Чтение изображения
+image = cv2.imread('Untitled.png', cv2.IMREAD_UNCHANGED)
 
-# отображаем контуры поверх изображения
-cv.drawContours(img, contours, -1, (255, 0, 0), 2, cv.LINE_AA, hierarchy, 0)
-cv.imshow('contours', img)
-cv.drawContours(img, contours, -1, (255, 0, 0), 2, cv.LINE_AA, hierarchy, 2)
-# выводим итоговое изображение в окно
-cv.imshow('All_con', img)
-cv.imshow('thresh', thresh)
-cv.waitKey()
-cv.destroyAllWindows()
+# Если изображение имеет альфа-канал, то его нужно убрать
+if image.shape[2] == 4:
+    image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+
+# Преобразование в оттенки серого
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Применение бинаризации (пороговой обработки) для выделения объектов
+_, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+# Нахождение контуров
+contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Масштаб: количество метров на пиксель (например, 0.01 метра на пиксель)
+scaling_factor = 0.01  # Можете настроить это значение в зависимости от вашего изображения
+
+# Перебор контуров и вычисление площади каждого из них в пикселях
+for contour in contours:
+    area_pixels = cv2.contourArea(contour)
+    # Конвертация площади из пикселей в квадратные метры
+    area_meters = area_pixels * (scaling_factor ** 2)
+    print(f'Площадь объекта: {area_meters} м²')
+
+    # Для визуализации, можно нарисовать контуры на изображении
+    cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
+
+# Показать изображение с контурами
+cv2.imshow('Contours', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
